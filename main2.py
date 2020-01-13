02d1950c72c75c6ad74814588310bbcd8a232c24
mastio1836 <prasetyotio775@gmail.com>
2020-01-13 12:15:54 +0700

    UAS1

---
+++ main2.py
@@ -0,0 +1,11 @@
+from core.baseapp import BaseApp
+from view.view_book import *
+
+class MainApp(BaseApp):
+    def __init__(selfself):
+        self.books = []
+
+        def list_book(self):
+            self.list_book()
+
+if      name        =="     main"   :
